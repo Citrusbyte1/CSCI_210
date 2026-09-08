05c50680ea765f0d37494073dcab3ec5dbe2ad50
Citrusbyte1 <brian.nguyen2107@gmail.com>
2026-09-08 15:58:00 -0700

    Added a player array

--- Lab 1 - Project 2/src/Project2.java
+++ Lab 1 - Project 2/src/Project2.java
@@ -1,6 +1,7 @@
 public class Project2 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("AAARRRRRGGGGHHHH");
-    }
+   public static void main(String[] var0) throws Exception {
+      Player[] players = new Player[20];
+      System.out.print(players);
+   }
 
 }
