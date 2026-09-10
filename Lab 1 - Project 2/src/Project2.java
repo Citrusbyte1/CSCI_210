9eb832fee74c81b86a94917708c2358941d5b1c1
Citrusbyte1 <brian.nguyen2107@gmail.com>
2026-09-10 15:42:01 -0700

    Added main class and code

--- Lab 1 - Project 2/src/Project2.java
+++ Lab 1 - Project 2/src/Project2.java
@@ -1,7 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Project2 {
-   public static void main(String[] var0) throws Exception {
+   public static void main(String[] args) {
       Player[] players = new Player[20];
-      System.out.print(players);
+      int playerCount = 0;
+
+      try {
+         Scanner fileScanner = new Scanner(new File("C:\\users\\ohama\\Desktop\\Palomar\\5) Fall 2026\\CSCI 210\\Lab 1 - Project 2\\src\\Players.txt"));
+
+         while (fileScanner.hasNext() && playerCount < players.length) {
+            String firstName = fileScanner.next();
+            String lastName = fileScanner.next();
+            int goals = fileScanner.nextInt();
+            int assists = fileScanner.nextInt();
+
+            players[playerCount] = new Player(firstName + " " + lastName, goals, assists);
+            playerCount++;
+         }
+
+         fileScanner.close();
+      } catch (FileNotFoundException exception) {
+         System.out.println("Unable to open Players.txt: " + exception.getMessage());
+         return;
+      }
+
+      for (Player player : players) {
+         if (player != null) {
+            System.out.println(player.toString());
+         }
+      }
    }
 
 }
