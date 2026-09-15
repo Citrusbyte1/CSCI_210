66ad9d6a8940859ecdc0c02076fb57dd66592d9e
Citrusbyte1 <brian.nguyen2107@gmail.com>
2026-09-15 16:14:48 -0700

    Renamed the sub-repository and added (given) base files from the assignment rubric.

---
+++ Module 2 - Homework 1/src/MyArrayList.java
@@ -0,0 +1,94 @@
+/**
+ * </h2>MyArrayList.java - Implementation of Java's ArrayList</h2>
+ * <p><b>Problem statement: Create a class which mimics several capabilities of Java's
+ *    ArrayList class</p>
+ * <p><b>Instance Variables:</b></p>
+ * <ul>
+ *   <li>elements[] String[] an array of Strings in the ArrayList</li>
+ *   <li>size (int) the number of elements in the ArrayList</li>
+ * </ul>
+ * @author <your name here>
+ * @version Module 2, Homework 1
+ */
+
+public class MyArrayList {
+
+    // Named constants
+    /** Capacity when no-argument constructor is used */
+    public static final int DEFAULT_CAPACITY = 10;
+
+    // Instance variables
+    private String[] elements = null;                  // the underlying array of this arraylist
+    private int size = 0;                              // the number of elements in the arraylist
+
+   /**
+    * No-argument constructor creates a default underlying array of length 10.
+    */
+    public MyArrayList() {
+        this(DEFAULT_CAPACITY);
+    }
+
+   /**
+    * Full constructor will set all the capacity to the lone parameter (must be non-negative)
+    * @param capacity the initial capacity of the arraylist (length of underlying array)
+    */
+    public MyArrayList(int capacity) {
+        elements = new String[Math.max(0, capacity)];
+        size = 0;
+    }
+
+   /**
+    * Show the size and capacity (the length of the underlying array) of the arraylist,
+    * followed (optionally) by the list of Strings in the arraylist.
+    * @param showDetail "true" if the list of Strings should be displayed
+    */
+     public void show(boolean showDetail) {
+         System.out.println("The size is " + size + " and the capacity is " + elements.length);
+         if (showDetail) {
+             System.out.println("\nHere are the Strings in arraylist");
+             for (int i = 0; i < size; i++) {
+                 System.out.println((i + 1) + ": " + elements[i]);
+             }
+         }
+     }
+
+   /**
+    * Change the capacity of the array to the new capacity (a parameter)
+    * @param capacity the new length of the underlying array
+    */
+     private void resize(int capacity) {
+
+         // Ensure that the new capacity isn't negative
+         if (capacity < 0) {
+             System.out.println("Internal error - negative capacity request: " + capacity);
+             return;
+         }
+
+         // If the requested capacity is the same as the current capacity, then just return
+         if (capacity == elements.length) {
+             return;
+         }
+
+         // System.out.println("Resizing from " + elements.length + " to " + capacity);
+
+         // Create a new array of the requested length
+         String[] newElements = new String[capacity];
+
+         // Copy the elements from the old array into the new array
+         size = Math.min(size, capacity);       // Size can never exceed capacity
+         if (size >= 0) {
+             System.arraycopy(elements, 0, newElements, 0, size);
+         }
+
+         // Start using the new array
+         // Print message if decreasing the capacity of the array
+         if (elements.length > capacity) {
+             System.out.println("Resizing array down from " + elements.length + " to " + capacity);
+         }
+         elements = newElements;        // Old array is reclaimed by the heap automatically
+    }
+
+   //*************************** Code to be added in this assignment *****************************/
+
+}
+
---
+++ Module 2 - Homework 1/src/Tester.java
@@ -0,0 +1,91 @@
+/**
+ * </h2>Tester.java - Test the methods of MyArrayList</h2>
+ * <p><b>Problem statement: Create several tests for the new MyArrayList class.</b></p>
+ * <p><b>Algorithm:</b></p>
+ * <ul>
+ *   <li>Create a new arraylist using the default constructor.  Add two Strings and show results.</li>
+ *   <li>Create a new arraylist with a capacity of 10.   Add more than 10 Strings and show results.</li>
+ *
+ *   <li>size (int) the number of elements in the arraylist</li>
+ *   <li>capacity (int) the size of the underlying array elements[].</li>
+ * </ul>
+ * @author <your name here>
+ * @version Module 2, Homework 1
+ */
+
+public class Tester {
+
+    public static void main(String[] args) {
+
+        //*********************************** Constructor Test **********************************/
+
+        // Create new arraylist using default constructor and using the one-parameter constructor.
+        System.out.println("Create an empty arraylist using the default constructor");
+        MyArrayList list1 = new MyArrayList();
+        list1.show(false);
+
+        System.out.println("\nCreate an empty arraylist using a constructor with capacity of 100");
+        MyArrayList list2 = new MyArrayList(100);
+        list2.show(false);
+
+        // Release memory
+        list1 = list2 = null;
+        //********************** Test getters, add, set, and at methods **********************/
+        /*
+        // Create an arraylist with default constructor, then fill with 1,000 lines.
+        System.out.println("\nCreate an arraylist and fill with 1,000 Strings.");
+        list1 = new MyArrayList();
+        for (int i = 0; i < 1000; i++) {
+            list1.add("String at " + i);
+        }
+        list1.show(false);
+        System.out.println("The value returned by getSize() is:     " + list1.getSize());
+        System.out.println("The value returned by getCapacity() is: " + list1.getCapacity() + "\n");
+
+        // Get the value at index 429, print old value, change to "New string"), and use the at
+        // method to show old and new values.
+        System.out.println("Old element at index 429: " + list1.at(429));
+        list1.set(429, "A new string " + 429);
+        System.out.println("New element at index 429: " + list1.at(429) + "\n");
+
+        // Try to access an element outside the valid range (valid range should be
+        // from 0 to (size - 1)
+        System.out.println("Test range checks for \"at\" and \"set\" methods.");
+        int index = list1.getSize();
+        System.out.println("Trying to access an element at a negative index");
+        list1.at(-1);
+        System.out.println("Try to access an element at position: " + index);
+        list1.at(index);
+        System.out.println("Trying to set an element at a negative index");
+        list1.set(-1, "Bad value");
+        System.out.println("Try to set an element at position: " + index);
+        list1.set(index, "Another bad value");
+        */
+
+        //********************* Test the second add and the remove ******************/
+        /*
+        // Try to insert a new line before each existing line, working backwards in the arraylist
+        for (int i = 999; i >= 0; i--) {
+            list1.add(i, "A new line " + i);
+        }
+        System.out.println("After inserting a new line before each line, elements 0 - 10 are:");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Element " + i + ": " + list1.at(i));
+        }
+        System.out.println("And here are the new stats:");
+        list1.show(false);
+
+        // Remove all but 20 elements
+        while (list1.getSize() > 20) {
+            list1.remove(20);
+        }
+        System.out.println("\nAfter removing all but 20 elements from the arraylist, the stats:");
+        list1.show(false);
+
+        // Run the trimToSize method and show the results.
+        System.out.println("\nAfter running the \"trimToSize\" method:");
+        list1.trimToSize();
+        list1.show(false);
+        */
+    }
+}
